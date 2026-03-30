f4494f76f3be21f9410e5fc2528f0a668f6c5b73
mdarifuzzaman0916-dev <mdarifuzzaman0916@gmail.com>
2026-03-30 21:09:25 +0600

    added stars section

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 const Stats = () => {
     return (
         <div className='bg-linear-to-l from-[#4F39F6] to-[#5b48e4d2]'>
-            <div className=' flex justify-around items-center w-10/12 mx-auto'>
-                <div>
-                    <h2 className='text-5xl font-bold text-white'>50K+</h2>
-                    <p>Active Users</p>
+            <div className='flex justify-around items-center w-10/12 mx-auto py-8'>
+                <div className='space-y-3'>
+                    <h2 className='text-4xl font-bold text-white'>50K+</h2>
+                    <p className='text-white'>Active Users</p>
                 </div>
-                <div>
-                    <h2>200+</h2>
-                    <p>Premium Tools</p>
+                <div className='space-y-3'>
+                    <h2 className='text-4xl font-bold text-white'>200+</h2>
+                    <p className='text-white'>Premium Tools</p>
                 </div>
-                <div>
-                    <h2>4.9</h2>
-                    <p>Rating</p>
+                <div className='space-y-3'>
+                    <h2 className='text-4xl font-bold text-white'>4.9</h2>
+                    <p className='text-white'>Rating</p>
                 </div>
             </div>
         </div>
